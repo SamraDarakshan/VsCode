dc298d36e7e6b7cbb4f431438fd6998fc644314c
SamraDarakshan <41741649+SamraDarakshan@users.noreply.github.com>
2019-05-02 19:32:07 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+
+console.log('request was made: ' + req.url);
+res.writeHead(200,{'Content-Type':'text plain'})
+res.write('hey samra');
+res.end();
+});
+
+//server.listen(3000,'127.0.0.1');
+server.listen(3000);
+console.log('Now listening to port 3000');
+//samra
